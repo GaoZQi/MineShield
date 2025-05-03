bad99da2efe9fd1c90a4b84dfaac97e4da03d75d
GaoZQi <Ga0ZQ1@outlook.com>
2025-05-03 19:28:37 +0800

    更新 MicaWindow 类以使用默认的 MicaStyle 样式

--- src/widget/MicaWindow.py
+++ src/widget/MicaWindow.py
@@ -8,7 +8,7 @@ class MicaWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         self.setGeometry(100, 100, 300, 200)
-        ApplyMica(self.winId(), MicaTheme.LIGHT, MicaStyle.ALT)
+        ApplyMica(self.winId(), MicaTheme.LIGHT, MicaStyle.DEFAULT)
         self.setContentsMargins(0, 0, 0, 0)
 
 
